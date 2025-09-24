7f509f23b4da6ea8f059d3d612ee2475488661ea
ethanratnofsky@gmail.com <ethan@kinetik.care>
2025-09-24 15:18:38 -0400

    change tilelayer

--- frontend/src/components/MapView/MapView.jsx
+++ frontend/src/components/MapView/MapView.jsx
@@ -84,8 +84,11 @@ const MapView = ({ vehicles = [], pickups = [], center = defaultCenter, zoom = d
         style={{ width: '100%', height: '100%' }}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+          subdomains={["a","b","c","d"]}
+          detectRetina={true}
+          maxZoom={20}
         />
 
         {isReady && (
